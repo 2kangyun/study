543034c08aaeb8cad52af497c5715a359bb9c8f1
kangyun <dkdpdh9@gmail.com>
2024-11-29 10:31:59 +0900

    두 수의 합 풀이

---
+++ programmers/C#/Lv.0/두 수의 합.cs
@@ -0,0 +1,14 @@
+// 문제 링크 https://school.programmers.co.kr/learn/courses/30/lessons/120802
+
+
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1; //결과 저장 변수
+        
+        answer = num1 + num2;
+        
+        return answer; // 결과 반환
+    }
+}
