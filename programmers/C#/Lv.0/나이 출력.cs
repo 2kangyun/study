a723749c81dbad929e71a63045002311a3deffef
kangyun <dkdpdh9@gmail.com>
2024-11-29 10:28:25 +0900

    나이 출력 풀이

---
+++ programmers/C#/Lv.0/나이 출력.cs
@@ -0,0 +1,14 @@
+// 문제 링크 https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+
+using System;
+
+public class Solution {
+    public int solution(int age) {
+        int answer = 0; // 결과 저장 변수
+        
+        answer = 2023 - age; // 나이 계산
+        
+        return answer; // 결과 반환
+    }
+}
