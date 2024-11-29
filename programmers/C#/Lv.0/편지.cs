3fceea749438ac1eae8062d3553f4b2774c39bd4
kangyun <dkdpdh9@gmail.com>
2024-11-29 14:49:56 +0900

    편지 풀이

---
+++ programmers/C#/Lv.0/편지.cs
@@ -0,0 +1,14 @@
+// 문제 링크 https://school.programmers.co.kr/learn/courses/30/lessons/120898
+
+
+using System;
+
+public class Solution {
+    public int solution(string message) {
+        int answer = 0; // 정답 변수 선언
+        
+        answer = message.Length * 2; // 정답 계산
+        
+        return answer; // 정답 반환
+    }
+}
