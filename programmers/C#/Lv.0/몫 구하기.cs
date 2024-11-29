c8e9e12e9a1f4ae3ae3344242695d2fcba3c2f65
kangyun <dkdpdh9@gmail.com>
2024-11-29 10:20:35 +0900

    몫 구하기 풀이

---
+++ programmers/C#/Lv.0/몫 구하기.cs
@@ -0,0 +1,14 @@
+// 문제 링크 https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0; // 결과 저장 변수 
+        
+        answer = num1 / num2; // 나누기
+        
+        return answer; // 결과 반환
+    }
+}
