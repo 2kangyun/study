4e1fba3263bbce77544196b24a6d76fcc61e504e
kangyun <clsrnfkdshfrhtlvek@naver.com>
2025-04-03 20:01:32 +0900

    문자열로 변환 풀이

---
+++ programmers/C#/Lv.0/문자열로 변환.cs
@@ -0,0 +1,14 @@
+// 문제 링크 https://school.programmers.co.kr/learn/courses/30/lessons/181845
+
+
+using System;
+
+public class Solution {
+    public string solution(int n) {
+        string answer = ""; 
+        
+        answer = n.ToString();
+        
+        return answer;
+    }
+}
