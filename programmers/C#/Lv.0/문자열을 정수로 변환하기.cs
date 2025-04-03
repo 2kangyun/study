69901f0a54273d4a80f179db5f53f78db003acd5
kangyun <clsrnfkdshfrhtlvek@naver.com>
2025-04-03 20:15:09 +0900

    문자열을 정수로 변환하기 풀이

---
+++ programmers/C#/Lv.0/문자열을 정수로 변환하기.cs
@@ -0,0 +1,14 @@
+// 문제 링크 https://school.programmers.co.kr/learn/courses/30/lessons/181848
+
+
+using System;
+
+public class Solution {
+    public int solution(string n_str) {
+        int answer = 0;
+        
+        answer = int.Parse(n_str);
+        
+        return answer;
+    }
+}
