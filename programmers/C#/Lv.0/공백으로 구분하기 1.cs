41e33952eca697761c3738b2855e4c47bbe38484
kangyun <clsrnfkdshfrhtlvek@naver.com>
2025-04-10 10:08:32 +0900

    공백으로 구분하기 1 풀이

---
+++ programmers/C#/Lv.0/공백으로 구분하기 1.cs
@@ -0,0 +1,14 @@
+// 문제 링크 https://school.programmers.co.kr/learn/courses/30/lessons/181869
+
+
+using System;
+
+public class Solution {
+    public string[] solution(string my_string) {
+        string[] answer = new string[] {};
+        
+        answer = my_string.Split(" ");
+        
+        return answer;
+    }
+}
