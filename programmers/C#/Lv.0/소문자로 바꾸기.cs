b912d3d7eec1d782263f4ba5aa8c02d15426f05c
kangyun <clsrnfkdshfrhtlvek@naver.com>
2025-04-10 10:28:15 +0900

    소문자로 바꾸기

---
+++ programmers/C#/Lv.0/소문자로 바꾸기.cs
@@ -0,0 +1,14 @@
+// 문제 링크 https://school.programmers.co.kr/learn/courses/30/lessons/181876
+
+
+using System;
+
+public class Solution {
+    public string solution(string myString) {
+        string answer = "";
+        
+        answer = myString.ToLower();
+        
+        return answer;
+    }
+}
